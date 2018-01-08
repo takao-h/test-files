37be10d85f74c30ca4cfce8e2d72e2e8a16349dd
takao-h <hayashi_takao@cyberbuzz.co.jp>
2018-01-09 00:07:42 +0900

    Form test  Please enter the commit message for your changes. Lines starting

--- test-for-react/src/App.js
+++ test-for-react/src/App.js
@@ -3,6 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  getInitialState() {
+   return {testValue: "Hello World!!!"};
+ }
+  hundleChange(event){
+    this.setState({
+      testValue: event.target.value
+    })
+  }
+  submitHundler(event){
+    event.preventDefault();
+    alert(this.state.testValue);
+  }
   render() {
     return (
       <div className="App">
@@ -13,6 +25,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <form onSubmit={this.submitHandler}>
+        <input type="text" value={this.state.testValue} onChange = {this.handleChange} />
+        <button type="submit">
+          送信！!!!
+        </button>
       </div>
     );
   }
